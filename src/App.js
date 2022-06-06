25373f89b539628f676c2fd87bc1966f7eaa8868
santiagosola94 <santiagosola94@gmail.com>
2022-06-06 14:13:11 -0300

    Desafio Contador con Boton

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <>
       <NavBar />
       <ItemListContainer greeting='Sportside Indumentaria Deportiva'/>
+      <ItemCount stock='10' valorInicial='1'/>
     </>
   );
 }
---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+function ItemCount(props) {
+    const {stock, valorInicial} = props
+    const [ count, modificarCont ] = useState(parseInt(valorInicial))
+    function sumar() {
+        if (count < stock) {
+            modificarCont(count + 1)
+        }
+    }
+
+    function restar(){
+        if (count > 0) {
+            modificarCont(count - 1)
+        }
+    }
+
+    function onAdd(){
+        console.log('el valor elegido es:', count)
+    }
+
+    return (
+        <>
+            <div className='contenedorContador'>
+                <div className='alMedio'>
+                    <h3>Producto 1</h3>
+                    <p>Stock: {stock}</p>
+                </div>
+                
+                <div className='contenedores'>
+                    <div className='flexContador'>
+                        <button onClick={restar}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='bi bi-arrow-left-circle' viewBox="0 0 16 16">
+                                <path fillRule='evenodd' d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+                            </svg>
+                        </button>
+                        <h4>{count}</h4>
+                        <button onClick={sumar}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className='bi bi-arrow-right-circle' viewBox="0 0 16 16">
+                                <path fillRule='evenodd' d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+                            </svg>
+                        </button>
+                    </div>
+                    <button onClick={onAdd}>Agregar al Carrito</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ItemCount
